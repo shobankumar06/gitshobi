275879d6052ecc6cadbbfae4bc6e54e93bdf2f43
shobankumar06 <shobankumar124>
2023-11-26 11:33:06 +0530

    added

---
+++ c-programming-main/c-programming-problems/Basic Input Output/printNewLineChar.c
@@ -0,0 +1,17 @@
+// C Program to Print Newline Character => '\n'
+
+
+//Header files
+#include<stdio.h>
+#include<conio.h>
+
+
+// Main Function Start
+int main(){
+
+    printf(" \\n "); 
+
+    getch(); 
+    return 0;
+}
+// Main Function End
